d962ff285c459873571f3c95ec6fca5d2f6aeeb2
AlbedoUniversal <Albedogforce@gmail.com>
2020-03-25 18:53:28 +0300

    first task

--- src/js/common.js
+++ src/js/common.js
@@ -1,2 +1,49 @@
-let add = (a, b) => a + b;
-console.log(add(2, 4));
+// ## JavaScript
+// ### Написать метод который собирает из объектного вида CSS в строку:
+// ```javascript
+// /*
+// 	interface IObjectCSS {
+// 	   [key: string]: number | string | IObjectCSS | [IObjectCSS],
+// 	}
+// */
+
+// const input = {
+//       body: {
+//       	    color: red,
+//       },
+
+//       '@media (max-width: 996px)': [
+//       	{
+// 		'.class-1': {
+// 			    'background-color': '#000',
+// 		}
+// 	}
+//       ]
+// };
+
+// const cssString = cssInJS(input);
+
+// console.log(cssString); // 'body {color: red;} @media (max-width: 996px) {.class-1 {background-color: #000}}'
+// ```
+
+const input = {
+  body: {
+    color: "red"
+  },
+  "@media (max-width: 996px)": {
+    "background-color": "#000"
+  }
+};
+
+function cssInJS(input) {
+  let strCss = "";
+  for (let item of Object.keys(input)) {
+    strCss += `${item}{;`;
+    for (let supItem of Object.keys(input[item])) {
+      strCss += `${supItem}:${input[item][supItem]};`;
+    }
+    strCss += `}`;
+  }
+  return strCss;
+}
+console.log(cssInJS(input));

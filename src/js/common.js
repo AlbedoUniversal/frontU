b6fe66ad12557f368f18243226f81132cb49a334
AlbedoUniversal <48028549+AlbedoUniversal@users.noreply.github.com>
2020-03-26 20:02:14 +0300

    old

--- src/js/common.js
+++ src/js/common.js
@@ -1,76 +1,46 @@
-// ## JavaScript
-// ### Написать метод который собирает из объектного вида CSS в строку:
-// ```javascript
-// /*
-// 	interface IObjectCSS {
-// 	   [key: string]: number | string | IObjectCSS | [IObjectCSS],
-// 	}
-// */
-
-// const input = {
-//       body: {
-//       	    color: red,
-//       },
-
-//       '@media (max-width: 996px)': [
-//       	{
-// 		'.class-1': {
-// 			    'background-color': '#000',
-// 		}
-// 	}
-//       ]
-// };
-
-// const cssString = cssInJS(input);
-
-// console.log(cssString); // 'body {color: red;} @media (max-width: 996px) {.class-1 {background-color: #000}}'
-// ```
-
-const input = {
-  body: {
-    color: "red"
-  },
-  "@media (max-width: 996px)": {
-    "background-color": "#000"
-  }
-};
-
-// function cssInJS(input) {
-//   let strCss = "";
-//   for (let item of Object.keys(input)) {
-//     strCss += `${item}{;`;
-//     for (let supItem of Object.keys(input[item])) {
-//       strCss += `${supItem}:${input[item][supItem]};`;
-//     }
-//     strCss += `}`;
-//   }
-//   return strCss;
-// }
-// console.log(cssInJS(input));
+// // ## JavaScript
+// // ### Написать метод который собирает из объектного вида CSS в строку:
 
 // const input = {
 //   body: {
-//     color: "red"
+//     color: 'red'
 //   },
-//   "@media (max-width: 996px)": {
-//     "background-color": "#000"
-//   }
+
+//   '@media (max-width: 996px)': [
+//     {
+//       '.class-1': {
+//         'background-color': '#000',
+//       }
+//     }
+//   ]
 // };
 
-function cssInJs(input) {
-  if (typeof input !== "object") {
-    // Только здесь нужно заморочиться, чтобы привести точно к строке, потому что если вернешь number, то все по пизде пойдет
-    return `${input};`;
-  }
+// function cssInJs(input) {
+//   if (typeof input !== "object") {
+//     // Только здесь нужно заморочиться, чтобы привести точно к строке, потому что если вернешь number, то все по пизде пойдет
+//     return `${input};`;
+//   }
 
-  let resStr = "{";
+//   let resStr = "{";
 
-  for (let i in input) {
-    const propVal = cssInJs(input[i]);
-    resStr += `${i}: ${propVal}`;
-  }
+//   for (let i in input) {
+//     const propVal = cssInJs(input[i]);
+//     resStr += `${i}: ${propVal}`;
+//   }
 
-  resStr += "}";
+//   resStr += "}";
 
-  return resStr;
+//   return resStr;
+// }
+// console.log(cssInJs(input));
+
+// ### Есть функция seq, логика её работы следующая seq(x => x +2)(x => x * 4)(4) // 18, необходимо написать реализацию этой функции (Если задачаслишком легка, то попытаться уместить решение в 100 символов или меньше без учета пробелов)
+
+
+function seq(x) {
+  return x + 2;
 }
+
+console.log(seq(2));
+
+// console.log(seq(x => x + 2)(x => x * 4)(4));

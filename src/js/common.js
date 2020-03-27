b3772b510cc7c191cb962ef16e27e808569df352
AlbedoUniversal <48028549+AlbedoUniversal@users.noreply.github.com>
2020-03-27 19:24:27 +0300

    task N2 done

--- src/js/common.js
+++ src/js/common.js
@@ -1,98 +1,98 @@
-// // ## JavaScript
-// // ### Написать метод который собирает из объектного вида CSS в строку:
-
-const input = {
-  body: {
-    color: "red"
-  },
-
-  "@media (max-width: 996px)": [
-    {
-      ".class-1": {
-        "background-color": "#000"
-      }
-    }
-  ]
-};
-
-function cssInJs(input) {
-  if (typeof input !== "object") {
-    return `${input}`;
-  }
-  let str = "{";
-  for (let i in input) {
-    if (Array.isArray(input[i])) {
-      let arrObjText = [];
-      input[i].forEach(elem => {
-        Object.keys(elem).forEach((k) => {
-          arrObjText.push(k);
-          Object.keys(elem[k]).forEach((y) => {
-            arrObjText.push(' ' + '{' + y + ':' + elem[k][y] + ';');
-          });
-        });
-        arrObjText = '{' + arrObjText.join('') + '}' + '}';
-      });
-      str += ` ${i} ${arrObjText}`;
-    } else {
-      const propVal = cssInJs(input[i]);
-      console.log(propVal);
-      str += `${i} :${propVal}`;
-    }
-    str += ";}";
-
-  }
-  return str;
-}
-
-console.log(cssInJs(input));
-
-
-
-// console.log(JSON.parse(input));
-// const cssString = cssInJS(input);
-
-// console.log(cssString); // 'body {color: red;} @media (max-width: 996px) {.class-1 {background-color: #000}}'
-// ```
+// // // ## JavaScript
+// // // ### Написать метод который собирает из объектного вида CSS в строку:
 
 // const input = {
 //   body: {
 //     color: "red"
 //   },
-//   "@media (max-width: 996px)": {
-//     "background-color": "#000"
-//   }
-// };
 
-// function cssInJS(input) {
-//   let strCss = "";
-//   for (let item of Object.keys(input)) {
-//     strCss += `${item}{;`;
-//     for (let supItem of Object.keys(input[item])) {
-//       strCss += `${supItem}:${input[item][supItem]};`;
+//   "@media (max-width: 996px)": [
+//     {
+//       ".class-1": {
+//         "background-color": "#000"
+//       }
 //     }
-//     strCss += `}`;
-//   }
-
-//   let resStr = "{";
-
-//   for (let i in input) {
-//     const propVal = cssInJs(input[i]);
-//     resStr += `${i}: ${propVal}`;
-//   }
+//   ]
+// };
 
 // function cssInJs(input) {
 //   if (typeof input !== "object") {
 //     return `${input}`;
 //   }
-
-//   let resStr = "{";
+//   let str = "{";
 //   for (let i in input) {
-//     const propVal = cssInJs(input[i]);
-//     resStr += `${i}: ${propVal}`;
-//   }
-//   resStr += "}";
+//     if (Array.isArray(input[i])) {
+//       let arrObjText = [];
+//       input[i].forEach(elem => {
+//         Object.keys(elem).forEach((k) => {
+//           arrObjText.push(k);
+//           Object.keys(elem[k]).forEach((y) => {
+//             arrObjText.push(' ' + '{' + y + ':' + elem[k][y] + ';');
+//           });
+//         });
+//         arrObjText = '{' + arrObjText.join('') + '}' + '}';
+//       });
+//       str += ` ${i} ${arrObjText}`;
+//     } else {
+//       const propVal = cssInJs(input[i]);
+//       console.log(propVal);
+//       str += `${i} :${propVal}`;
+//     }
+//     str += ";}";
 
-//   return resStr;
+//   }
+//   return str;
 // }
 
 // console.log(cssInJs(input));
+
+
+
+// // console.log(JSON.parse(input));
+// // const cssString = cssInJS(input);
+
+// // console.log(cssString); // 'body {color: red;} @media (max-width: 996px) {.class-1 {background-color: #000}}'
+// // ```
+
+// // const input = {
+// //   body: {
+// //     color: "red"
+// //   },
+// //   "@media (max-width: 996px)": {
+// //     "background-color": "#000"
+// //   }
+// // };
+
+// // function cssInJS(input) {
+// //   let strCss = "";
+// //   for (let item of Object.keys(input)) {
+// //     strCss += `${item}{;`;
+// //     for (let supItem of Object.keys(input[item])) {
+// //       strCss += `${supItem}:${input[item][supItem]};`;
+// //     }
+// //     strCss += `}`;
+// //   }
+
+// //   let resStr = "{";
+
+// //   for (let i in input) {
+// //     const propVal = cssInJs(input[i]);
+// //     resStr += `${i}: ${propVal}`;
+// //   }
+
+// // function cssInJs(input) {
+// //   if (typeof input !== "object") {
+// //     return `${input}`;
+// //   }
+
+// //   let resStr = "{";
+// //   for (let i in input) {
+// //     const propVal = cssInJs(input[i]);
+// //     resStr += `${i}: ${propVal}`;
+// //   }
+// //   resStr += "}";
+
+// //   return resStr;
+// // }
+
+// // console.log(cssInJs(input));
